c365f404603cd92c040d91b6553069b8b5a17f43
jbau93 <jdbv93@gmail.com>
2021-05-09 16:34:03 -0500

    [feat]:repository feedback

---
+++ src/main/java/com/survey/restaurant/business/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package com.survey.restaurant.business.repository;
+
+import com.survey.restaurant.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
+}
